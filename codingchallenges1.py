8c488469329c670b4dde3f57762bf97f43d55eb8
hollie-w <hollie.walton@hotmail.com>
2023-08-13 11:42:04 +0100

    first commit

---
+++ codingchallenges1.py
@@ -0,0 +1,7 @@
+import math
+radians = int(input("What is your angle in radians?"))
+degrees = radians * (180/math.pi)
+msg = "Your angle is {} degrees". format(degrees)
+print(msg)
+#converts user's angle in radians into degrees
+
